0d012d7e0704113d15f70cdfec5409247d8ef309
RosalBez <rosalbezuijen@hotmail.com>
2023-04-04 17:13:13 +0200

    uitwerkingen

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -20,15 +20,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6
 
-let amountOfCumlaude =  0
-
-for (let i = 0; i < grades.length ; i++) {
-    if (grades[i] >= 8) {
-        amountOfCumlaude = amountOfCumlaude + 1
+function cumLaude (gradeArray, minimumNumber) {
+    let amountOfCumLaude = []
+    for (let i = 0; i < gradeArray.length; i++) {
+        if (gradeArray[i] >= minimumNumber) {
+            amountOfCumLaude.push(gradeArray[i])
+        }
     }
+    return amountOfCumLaude.length
 }
-const resultAmountOfCumlaude = amountOfCumlaude
-console.log(resultAmountOfCumlaude)
+    console.log(cumLaude([6, 4, 5], 8))  // bij gradeArray kun je nu ieder willekeurig array meegeven als argument dus kun je alle gewenste outputs krijgen
+
 
 
 
@@ -38,41 +40,35 @@ console.log(resultAmountOfCumlaude)
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomsten:
-
-// cumLaude(grades) geeft 6
-function cumLaude(grades) {
-     return resultAmountOfCumlaude
-}
-console.log(cumLaude(grades))
+// cumLaude(grades) geeft 6 //
 
 // cumLaude([6, 4, 5]) geeft 0
-const gradesSecondClass = [6, 4, 5]
-
-function CumLaudeSecond(gradesSecondClass) {
-    for (let i = 0; i < gradesSecondClass.length ; i++) {
-        if (gradesSecondClass[i] >= 8) {
-            amountOfCumlaude = amountOfCumlaude + 1
-        }
-        return resultAmountOfCumlaude
-    }
-}
-
-console.log(cumLaude(gradesSecondClass))
-
-
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
-
-/* Opdracht  2: Gemiddeld cijfer */
+//* Opdracht  2: Gemiddeld cijfer */
 
 /* 2a: Script schrijven  */
 // De studenten-administratie moet ieder blok opnieuw berekenen wat het gemiddelde eindcijfer is, maar we beginnen met de grades array van hierboven.
 // Schrijf de stapjes om dit te kunnen berekenen eerst uit en vraag jezelf de volgende dingen af:
 // * Hoe wordt een gemiddelde berekend?
+//>>> gemiddled is alles cijfers samen gedeeld door het aantal cijfers in de reeks <<<<<<
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
+//>>>> de length is nodig zodat alle cijfers worden meegenomen en het totaal van de lenght (hoeveel cijfers zijn er) <<<<<
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
+//>>> met een for loop via grades.lenght kun je in iedergeval alle cijfers langs lopen, deze cijfers moeten bij elkaar opgeteld worden en uiteindelijk gedeeld worden door de lenght
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6.642857142857143
+
+function averageGrade (gradeArray) {                //dit is ook 2b want gradeArray zorgt ervoor dat je verschillende arrays als argument mee kan geven)
+    let totalGrade = 0
+    for (let i = 0; i < gradeArray.length; i++) {
+        totalGrade += gradeArray[i]
+    }
+    const averageNumber = totalGrade / gradeArray.length
+    return averageNumber
+}
+console.log(averageGrade(grades).toFixed(2))  // tofix zorgt voor afronding op 2 decimalen (opdr 2c) die'2'in dit geval geeft aan dat je 2 decimalen wilt je kunt hier dus ook 4-5etc neerzetten
+
 
 
 /* 2b: Omschrijven tot een herbruikbare functie */
@@ -103,6 +99,19 @@ console.log(cumLaude(gradesSecondClass))
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 9
+
+
+
+function highestNumber (gradeArray) {               //3a en 3b door parameter gradeArray mee te geven kun je iedere array in deze functie kwijt
+    let highestGrade = 0
+    for (let i = 0; i < gradeArray.length; i++) {
+        if (gradeArray[i] > highestGrade)
+            highestGrade = gradeArray[i]
+    }
+    return highestGrade
+}
+
+console.log(highestNumber(grades))
 
 
 /* 3b: Omschrijven tot een herbruikbare functie */
